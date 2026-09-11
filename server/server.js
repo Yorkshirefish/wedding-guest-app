850b681d1400938583da37090e829f7b588a4b71
Will Fisher <willem.fisher98@gmail.com>
2026-09-11 20:29:09 +0100

    Setting up basic server.js file

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT||5002;
+
+app.get("/", (req, res) => {
+    res.json("hello")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}.`);
+    console.log(`http://localhost:${PORT}/`)
+});
+
