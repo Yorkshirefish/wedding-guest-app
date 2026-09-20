63ab5139ddf04639ade4f5432521d0113e3e2354
Will Fisher <willem.fisher98@gmail.com>
2026-09-20 18:33:28 +0100

    Styling the GuestCount Icons

--- client/src/components/core/guestCount.jsx
+++ client/src/components/core/guestCount.jsx
@@ -2,7 +2,7 @@ export default function GuestCount() {
     return (
         <div id="guest-count" className="flex flex-row gap-5">
             <div className="count-cont">
-                <div>
+                <div className=" icon-cont bg-green-100 text-green-800">
                     <i className="fa-light fa-people-group"></i>
                 </div>
                 <div>
@@ -11,7 +11,7 @@ export default function GuestCount() {
                 </div>
             </div>
             <div className="count-cont">
-                <div>
+                <div className="icon-cont bg-pink-100 text-pink-700">
                     <i className="fa-regular fa-venus"></i>
                 </div>
                 <div>
@@ -20,7 +20,7 @@ export default function GuestCount() {
                 </div>
             </div>
             <div className="count-cont">
-                <div>
+                <div className="icon-cont bg-blue-100 text-blue-800">
                     <i className="fa-regular fa-mars-stroke"></i>
                 </div>
                 <div>
